8b1bba9013668a6428d58934bd20cc0c328a2269
Helen Pickavance <helenpjobs@gmail.com>
2023-04-08 15:56:08 +0100

    Feature branch 1 - init commit

--- src/main/java/com/ellohez/Main.java
+++ src/main/java/com/ellohez/Main.java
@@ -2,7 +2,7 @@ package com.ellohez;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dev branch");
+        System.out.println("Feature 1 branch");
         System.out.println("Hello world!");
     }
 }
