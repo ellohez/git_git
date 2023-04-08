432860b7d3a4a3669e5974c861429233a5949542
Helen Pickavance <helenpjobs@gmail.com>
2023-04-08 15:54:19 +0100

    Dev branch init commit

--- src/main/java/com/ellohez/Main.java
+++ src/main/java/com/ellohez/Main.java
@@ -2,7 +2,7 @@ package com.ellohez;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main branch");
+        System.out.println("Dev branch");
         System.out.println("Hello world!");
     }
 }
