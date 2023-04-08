3f11f18794cac2da482e48aa80b078835d64f24f
Helen Pickavance <helenpjobs@gmail.com>
2023-04-08 16:29:55 +0100

    Feature 3 branch

---
+++ src/main/java/com/ellohez/Feature_3.java
@@ -0,0 +1,13 @@
+package com.ellohez;
+
+public class Feature_3 {
+    protected int test_class = 1;
+
+    public int getTest_class() {
+        return test_class;
+    }
+
+    public void setTest_class(int test_class) {
+        this.test_class = test_class;
+    }
+}
